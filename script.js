09ad35b7ccb8383fefc313198627a2aea145771f
Gabriel Kaique <gabrielkaique_contato@hotmail.com>
2022-10-03 20:33:02 -0300

    Adicionando functions de lógica

--- script.js
+++ script.js
@@ -29,9 +29,9 @@ function adicionar(){
 
     /* 1 - Mostrar total de números XXXXX
        2 - Mostrar o maior valor
-       3 - Mostrar o menor valor
+       3 - Mostrar o menor valor XXXX
        4 - mostrar a soma dos valores XXXXXX
-       5 - mostrar a média dos valores obs: mostrar somente 2 casas decimais!
+       5 - mostrar a média dos valores obs: mostrar somente 2 casas decimais! XXXX
     */
 function calcula(){
     var total = userNumbers.length;
@@ -39,7 +39,7 @@ function calcula(){
     var resp = document.createElement('p')
 
     resp.innerHTML = ''
-    resp.innerHTML = `Você adicionou ${total} números <br> A soma dos seus números é ${somar()} <br>`
+    resp.innerHTML = `Você adicionou ${total} números <br> A soma dos seus números é ${somar()} <br> A média dos valores é ${media()} <br> O maior valor da lista é ${maiorMenor('b')} <br> O menor valor da lista é ${maiorMenor('a')}`
     campoR.appendChild(resp);
 }
 
@@ -48,4 +48,22 @@ function somar(){
    for(let pos = 0; pos < userNumbers.length ; pos++){
     c += userNumbers[pos]
    } return c;
+}
+
+function media(){
+    var medUserNumbers = somar()/userNumbers.length
+    return medUserNumbers;
+}
+
+function maiorMenor(opt){
+    userNumbers.sort(function ordena(a,b){return a-b;});
+
+    var menorValor = Number(userNumbers[0]);
+    var maiorValor = Number(userNumbers[userNumbers.length-1]);
+
+    if (opt == 'a'){
+        return menorValor;
+    }else if(opt == 'b'){
+        return maiorValor;
+    }
 }
